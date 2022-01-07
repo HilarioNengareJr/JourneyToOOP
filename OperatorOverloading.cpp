bcd056f4d8e6d272b1dbf7abcc0ede89a725dd84
HilarioNengareJr <38634516+HilarioNengareJr@users.noreply.github.com>
2022-01-07 21:57:31 +0200

    Update OperatorOverloading.cpp

--- OperatorOverloading.cpp
+++ OperatorOverloading.cpp
@@ -11,6 +11,7 @@ class CVector {
         };
         CVector(int, int);
         CVector operator + (CVector);
+        CVector& operator= (const CVector&);
 };
 CVector::CVector(int a, int b) {
         x = a;
@@ -22,6 +23,13 @@ CVector CVector::operator + (CVector param) {
         temp.y = y + param.y;
         return (temp);
 }
+CVector& CVector::operator= (const CVector& param)
+{
+ x=param.x;
+ y=param.y;
+ return *this;
+} 
+
 int main() {
         CVector a(3, 1);
         CVector b(1, 2);
